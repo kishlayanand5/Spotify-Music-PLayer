07b506b5c08ed00f4be25d8f0133a51715815719
kishlayanand5 <84984827+kishlayanand5@users.noreply.github.com>
2023-05-25 13:39:34 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./Header.css";
+
+const Header = () => {
+  return <img className="header-img" src="https://imgix.bustle.com/uploads/image/2018/10/5/32a337c4-a6e9-4c0e-b512-010bb98e0049-spotify-logo.png?w=1020&h=574&fit=crop&crop=faces&auto=format%2Ccompress" alt="spotify-logo"/>;
+};
+
+export default Header;
